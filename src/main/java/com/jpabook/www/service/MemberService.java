9e7055710b32ff0224c1a856ebe54346dbce5696
kimminwoo6039 <qkqhemforhs1@gmail.com>
2023-01-19 07:16:08 +0900

    JPA

---
+++ src/main/java/com/jpabook/www/service/MemberService.java
@@ -0,0 +1,59 @@
+package com.jpabook.www.service;
+
+import com.jpabook.www.domain.Member;
+import com.jpabook.www.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true) //// readonly 사용하면 DB 성능 UP 읽기용 모드
+@RequiredArgsConstructor // 이것을 사용하면 final 생성자를 이용해서 기본생성자 생성해줌. 필요한것은 필드 만들어서 사용
+public class MemberService {
+
+
+    private final MemberRepository memberRepository; // 필드 생성자 비추천
+
+//    @Autowired // 생성자 인젝션 ( 많이 추천 ) 어노테이션 삭제가능 // 생성자 한개면 자동주입
+//    public MemberService(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+
+    /**
+    * 회원가입
+    * */
+    @Transactional // 등록하는 부분에서 readOnly = true 하면 값이 안바뀜
+    public Long join(Member member) {
+        validateDuplicateMember(member); // 중복회원검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    /**
+     * 회원 중복 검증
+     * */
+     public void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName()); // DB 에 name 값을 유니크제약조건을 잡아줘야함
+        if(!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    /**
+     * 회원 전체 조회
+     * */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    /**
+     * 회원 한명 조회
+     * */
+    @Transactional(readOnly = true)
+    public Member findOne(Long id) {
+        return memberRepository.findOne(id);
+    }
+}
---
+++ src/test/java/com/jpabook/www/service/MemberServiceTest.java
@@ -0,0 +1,64 @@
+package com.jpabook.www.service;
+
+import com.jpabook.www.domain.Member;
+import com.jpabook.www.repository.MemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class) // 스프링 엮어서 할래
+@SpringBootTest
+@Transactional // 기본값 롤백임.. 없애줘야ㅕ함
+public class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    EntityManager em;
+
+
+    @Test
+//    @Rollback(false) // 이거안쓰면 DB 에 안들어감..
+    public void 회원가입() throws Exception {
+        //given
+        Member member = new Member();
+        member.setName("kim");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        em.flush(); // 영속성 저장해줘야함.
+        assertEquals(member, memberRepository.findOne(saveId));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void 중복_회원_예외() throws Exception {
+        //given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        //when
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        //when
+
+        memberService.join(member1);
+        memberService.join(member2);
+        //thena
+        fail("예외가 발생해야 한다.");
+    }
+
+}
